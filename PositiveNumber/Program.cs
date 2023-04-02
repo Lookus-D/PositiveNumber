c3e8f1e09e5e338a369f8e9b04a69929be2a6cfa
Łukasz Dudek <lukdud@gmail.com>
2023-04-02 23:19:09 +0200

    Add project files.

---
+++ PositiveNumber/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PositiveNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("App to check that number is positive or negative");
+            Console.WriteLine("Write any number");
+            string number = Console.ReadLine();
+            int chosenNumber = 0;
+            Int32.TryParse(number, out chosenNumber);
+
+            switch (chosenNumber) 
+            {
+                case >=0:
+                    Console.WriteLine("Your number is positive");
+                        break;
+                case <0:
+                    Console.WriteLine("Your number is negative");
+                        break;
+            }
+        }
+        
+    }
+
+}
